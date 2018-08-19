9efb5a0939c94f3b12582c50c60afb14ed375a21
Leon <leon.karho.chung@gmail.com>
2018-08-19 16:20:50 -0400

    todo: modal

--- client/src/components/main/ContentTable.js
+++ client/src/components/main/ContentTable.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react'
-import BizModal from '../modal/BizModal';
 
 class ContentTable extends Component {
-  constructor() {
-    super()
-    this.state = {
-      isOpen: false
-    }
-  }
-
-  openModal = () => {
-    this.setState({
-      isOpen: true
-    })
-  }
-
-  closeModal = () => {
-    this.setState({
-      isOpen: false
-    })
-  }
-
   fetchBiz = () => {
     const { businesses } = this.props
     if(businesses.length){
       return businesses.map((biz, idx) => {
         return (
-          <tr key={idx} onClick={this.openModal} >
+          <tr key={idx}>
             <th>{idx+1}</th>
             <td>{biz.name}</td>
             <td>{biz.location.address1}</td>
@@ -45,11 +25,6 @@ class ContentTable extends Component {
             </tbody>
           </table>
         </div>
-        <BizModal 
-          businesses={this.props.businesses}
-          isOpen={this.state.isOpen} 
-          closeModal={this.closeModal}
-        />
       </section>
     )
   }
