a2825afd54f9242de917ac6e3e6fd63c1b43c6d8
Leon <leon.karho.chung@gmail.com>
2018-09-13 18:09:08 -0400

    create Favourites component

---
+++ client/src/components/main/Favourites.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import ContentTable from './ContentTable';
+
+class Favourites extends Component {
+  render() {
+    return (
+      <div>
+        <div>Favourites</div>
+      </div>
+    )
+  }
+}
+
+export default Favourites
